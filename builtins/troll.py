666a282cf8d6bde550a515fee0f26219dd911232
Zahtec <32808999+zahtec@users.noreply.github.com>
2022-06-30 08:31:48 -0700

    testing signing

--- builtins/troll.py
+++ builtins/troll.py
@@ -1,7 +1,11 @@
-from typing import Literal
+from typing import Literal, Iterable
 
 SupportsWrite = tuple
 
 
 def print(*values: object, sep: str | None = ..., end: str | None = ..., file: SupportsWrite[str] | None = ..., flush: Literal[False] = ...) -> None:
+    ...
+
+
+def enumerate(iterable: Iterable[enumerate], start: int = ...) -> None:
     ...
